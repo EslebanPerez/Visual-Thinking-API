96b1b99d339bb06180be845e71a4a44b84c3593e
EslebanPerez <eslebanperez@gmail.com>
2022-05-06 18:39:18 -0500

    Agregando prueba para obtener el correo de los estudiantes certificados

--- test/controllers/studentController.test.js
+++ test/controllers/studentController.test.js
@@ -1,8 +1,12 @@
-const studentController = require ("./../../lib/controllers/studentController")
+const studentController = require ("./../../lib/controllers/studentController");
 
 describe("Test Para Student Controller", () => {
     test("Test 1: Obtener a todos los estudiantes",()=>{
         const students =  studentController.getAllStudents();
         expect(students.length).toBe(51);  
+    });
+    test("test 2: Obtner el correo de estudiantes certificados",()=>{
+        const studentsCertificated = studentController.getEmailStudentsCertificated();
+        expect(studentsCertificated).toContain("Todd@visualpartnership.xyz");
     });
 });
