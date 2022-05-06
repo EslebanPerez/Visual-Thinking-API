f0cbc87475cc4ebb4e20b502101f760c816251fd
EslebanPerez <eslebanperez@gmail.com>
2022-05-05 22:39:04 -0500

    Agregando prueba para la clase que lee la DB

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,12 +1,12 @@
-const Reader = require("./../../lib/utils/Reader")
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Test for ExplorerService", () => {
-     test("Test 1: Verificando que la base de datos no este vacía ", () =>{
-         const students = Reader.readJsonFile("visualpartners1.json"); 
-         expect(students).not.toBeUndefined();
-     });
-     test("Test 2: Verificar el número de students en la BD ", () =>{
-         const students = Reader.readJsonFile("visualpartners.json"); 
-         expect(students.length).toBe(51);
-     });
+    test("Test 1: Verificando que la base de datos no este vacía ", () =>{
+        const students = Reader.readJsonFile("visualpartners1.json"); 
+        expect(students).not.toBeUndefined();
+    });
+    test("Test 2: Verificar el número de students en la BD ", () =>{
+        const students = Reader.readJsonFile("visualpartners.json"); 
+        expect(students.length).toBe(51);
+    });
 });
