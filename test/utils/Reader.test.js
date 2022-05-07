465175afa3bdafe12682f6411590d60b4c98fc79
EslebanPerez <eslebanperez@gmail.com>
2022-05-06 20:50:19 -0500

    Actualizando prueba que lee la DB

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("./../../lib/utils/Reader");
 
 describe("Test for ExplorerService", () => {
     test("Test 1: Verificando que la base de datos no este vacía ", () =>{
-        const students = Reader.readJsonFile("visualpartners1.json"); 
+        const students = Reader.readJsonFile("visualpartners.json"); 
         expect(students).not.toBeUndefined();
     });
     test("Test 2: Verificar el número de students en la BD ", () =>{
